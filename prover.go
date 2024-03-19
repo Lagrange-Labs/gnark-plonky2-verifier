37373fb8e41da438c396be2b256bcbb813c3a41a
Steven Gu <asongala@163.com>
2024-03-19 21:25:38 +0800

    Add Groth16 prover CLI.

---
+++ prover.go
@@ -0,0 +1,140 @@
+// Groth16 prover CLI
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/constraint"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/profile"
+	"github.com/succinctlabs/gnark-plonky2-verifier/types"
+	"github.com/succinctlabs/gnark-plonky2-verifier/variables"
+	"github.com/succinctlabs/gnark-plonky2-verifier/verifier"
+)
+
+// Main entry point
+func main() {
+	// Parse the CLI arguments.
+	inDir := flag.String("in-dir", "", "Input wrapped proof dir path")
+	outProof := flag.String("out-proof", "", "Output Groth16 proof file path")
+	outContract := flag.String("out-contract", "", "Output Solidity contract file path")
+	profileCircuit := flag.Bool("profile", false, "Profile the circuit")
+	dummySetup := flag.Bool("dummy", false, "Use the dummy setup")
+
+	flag.Parse()
+
+	fmt.Print("Running Groth16 prover\n")
+	fmt.Printf("InDir: %s, OutProof: %s, OutContract: %s\n", *inDir, *outProof, *outContract)
+	fmt.Printf("ProfileCircuit: %t, DummySetup: %t\n", *profileCircuit, *dummySetup)
+
+	// Run the prover.
+	runProver(*inDir, *outProof, *outContract, *profileCircuit, *dummySetup)
+}
+
+// Run the Groth16 prover.
+func runProver(inDir string, outProof string, outContract string, profileCircuit bool, dummySetup bool) {
+	// Read the circuit data and proof from the input dir.
+	commonCircuitData := types.ReadCommonCircuitData(inDir + "/common_circuit_data.json")
+	proofWithPis := variables.DeserializeProofWithPublicInputs(types.ReadProofWithPublicInputs(inDir + "/proof_with_public_inputs.json"))
+	verifierOnlyCircuitData := variables.DeserializeVerifierOnlyCircuitData(types.ReadVerifierOnlyCircuitData(inDir + "/verifier_only_circuit_data.json"))
+
+	// Build the circuit.
+	// TODO: load the circuit from previous serialization.
+	r1cs := buildCircuit(profileCircuit, commonCircuitData, proofWithPis, verifierOnlyCircuitData)
+
+	// Generate the proof.
+	generateProof(dummySetup, outProof, outContract, proofWithPis, verifierOnlyCircuitData, r1cs)
+}
+
+// Build the input circuit.
+func buildCircuit(profileCircuit bool, commonCircuitData types.CommonCircuitData, proofWithPis variables.ProofWithPublicInputs, verifierOnlyCircuitData variables.VerifierOnlyCircuitData) constraint.ConstraintSystem {
+	circuit := verifier.ExampleVerifierCircuit{
+		Proof:                   proofWithPis.Proof,
+		PublicInputs:            proofWithPis.PublicInputs,
+		VerifierOnlyCircuitData: verifierOnlyCircuitData,
+		CommonCircuitData:       commonCircuitData,
+	}
+
+	var p *profile.Profile
+	if profileCircuit {
+		p = profile.Start()
+	}
+
+	var builder frontend.NewBuilder
+	builder = r1cs.NewBuilder
+
+	// Compile the input circuit to CS.
+	r1cs, err := frontend.Compile(ecc.BN254.ScalarField(), builder, &circuit)
+	if err != nil {
+		fmt.Println("error in building circuit", err)
+		os.Exit(1)
+	}
+
+	if profileCircuit {
+		// Print the profile result.
+		p.Stop()
+		p.Top()
+		println("r1cs.GetNbCoefficients(): ", r1cs.GetNbCoefficients())
+		println("r1cs.GetNbConstraints(): ", r1cs.GetNbConstraints())
+		println("r1cs.GetNbSecretVariables(): ", r1cs.GetNbSecretVariables())
+		println("r1cs.GetNbPublicVariables(): ", r1cs.GetNbPublicVariables())
+		println("r1cs.GetNbInternalVariables(): ", r1cs.GetNbInternalVariables())
+	}
+
+	return r1cs
+}
+
+// Generate the Groth16 proof.
+func generateProof(dummySetup bool, outProof string, outContract string, proofWithPis variables.ProofWithPublicInputs, verifierOnlyCircuitData variables.VerifierOnlyCircuitData, r1cs constraint.ConstraintSystem) {
+	var pk groth16.ProvingKey
+	var vk groth16.VerifyingKey
+	var err error
+
+	assignment := verifier.ExampleVerifierCircuit{
+		Proof:                   proofWithPis.Proof,
+		PublicInputs:            proofWithPis.PublicInputs,
+		VerifierOnlyCircuitData: verifierOnlyCircuitData,
+	}
+
+	// Run trusted setup.
+	fmt.Println("Running circuit setup", time.Now())
+	if dummySetup {
+		fmt.Println("Using dummy setup")
+		pk, err = groth16.DummySetup(r1cs)
+	} else {
+		fmt.Println("Using real setup")
+		pk, vk, err = groth16.Setup(r1cs)
+	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if outContract != "" {
+		// Output the Solidity contract.
+		fSolidity, _ := os.Create(outContract)
+		err = vk.ExportSolidity(fSolidity)
+	}
+
+	fmt.Println("Generating witness", time.Now())
+	witness, _ := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+
+	fmt.Println("Creating proof", time.Now())
+	proof, err := groth16.Prove(r1cs, pk, witness)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Output the Groth16 proof.
+	fProof, _ := os.Create(outProof)
+	proof.WriteTo(fProof)
+	fProof.Close()
+}
